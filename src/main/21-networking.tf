b965178b33c88ef75de9bcef7bdaed1f814713b0
Alessio Creo <alessio.creo@keypartner.com>
2023-02-15 18:03:51 +0100

    feat: Add networking with Community template

---
+++ src/main/21-networking.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = "${var.app_name}-${var.env}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  one_nat_gateway_per_az = true
+
+  enable_dns_hostnames = true  
+
+  enable_flow_log = true
+  create_flow_log_cloudwatch_iam_role= true  
+  create_flow_log_cloudwatch_log_group = true
+
+  tags = var.tags
+}
