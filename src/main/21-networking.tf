3bb1d01b0496c48a88af3c4425a6942173d5d8f4
Alessio Creo <alessio.creo@keypartner.com>
2023-02-15 18:09:40 +0100

    refactor: Change version declaration

--- src/main/21-networking.tf
+++ src/main/21-networking.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "3.19.0"
 
   name = "${var.app_name}-${var.env}-vpc"
@@ -9,13 +9,13 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway     = true
   one_nat_gateway_per_az = true
 
-  enable_dns_hostnames = true  
+  enable_dns_hostnames = true
 
-  enable_flow_log = true
-  create_flow_log_cloudwatch_iam_role= true  
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_iam_role  = true
   create_flow_log_cloudwatch_log_group = true
 
   tags = var.tags
