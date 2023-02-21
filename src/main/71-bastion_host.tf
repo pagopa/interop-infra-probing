c0b345b51318f601da0e45042d872d66b4c371c6
Alessio Creo <alessio.creo@keypartner.com>
2023-02-21 17:14:52 +0100

    fix: Applied terraform fmt

--- src/main/71-bastion_host.tf
+++ src/main/71-bastion_host.tf
@@ -4,17 +4,17 @@ module "ec2_instance" {
 
   name = "bastion-host-${var.env}"
 
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t3.micro"
-  key_name               = data.aws_key_pair.this.key_name
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t3.micro"
+  key_name                    = data.aws_key_pair.this.key_name
   associate_public_ip_address = true
-  monitoring             = true
-  subnet_id              = module.vpc.public_subnets[0]
+  monitoring                  = true
+  subnet_id                   = module.vpc.public_subnets[0]
 
 }
 
 data "aws_ami" "amazon_linux" {
-  most_recent      = false
+  most_recent = false
   filter {
     name   = "name"
     values = ["amzn2-ami-kernel-5.10-hvm-2.0.20230207.0-x86_64-gp2"]
