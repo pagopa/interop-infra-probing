a304e10bce118bd9a996ba79f4944c60f4a8b902
Alessio Creo <alessio.creo@keypartner.com>
2023-02-21 15:23:42 +0100

    refactor: Use specific variable for database subnets

--- src/main/21-networking.tf
+++ src/main/21-networking.tf
@@ -6,8 +6,9 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
-  private_subnets = concat(var.eks_control_plane_subnets,var.eks_workload_subnets,var.database_subnets,)
+  private_subnets = concat(var.eks_control_plane_subnets,var.eks_workload_subnets)
   public_subnets  = var.app_public_subnets
+  database_subnets = var.database_subnets
 
   enable_nat_gateway     = true
   one_nat_gateway_per_az = true
