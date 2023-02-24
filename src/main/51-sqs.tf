b8dbe41e35d0d73ba479d9e6fcdc5d1af5a4920c
Alessio Creo <alessio.creo@keypartner.com>
2023-02-24 12:16:55 +0100

    feat(SQS): Add new queues

--- src/main/51-sqs.tf
+++ src/main/51-sqs.tf
@@ -2,6 +2,32 @@ module "sqs" {
   source  = "terraform-aws-modules/sqs/aws"
   version = "4.0.1"
 
-  name       = "${var.app_name}-bucket_reader_queue-${var.env}"
+  name       = "eservice_registry_queue"
   fifo_queue = true
+}
+
+module "sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "4.0.1"
+
+  name = "eservice_polling_queue"
+
+}
+
+
+module "sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "4.0.1"
+
+  name = "eservice_polling_result_queue"
+
+}
+
+
+module "sqs" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "4.0.1"
+
+  name = "eservice_telemetry_result_queue"
+
 }
