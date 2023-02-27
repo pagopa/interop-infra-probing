e6fac99a31de0cea99bfa69207d748d5be95e287
alessio-creo <124075593+alessio-creo@users.noreply.github.com>
2023-02-27 09:34:04 +0100

    feat: Create queues (#10)

--- src/main/51-sqs.tf
+++ src/main/51-sqs.tf
@@ -1,7 +1,33 @@
-module "sqs" {
+module "sqs_registry_queue" {
   source  = "terraform-aws-modules/sqs/aws"
   version = "4.0.1"
 
-  name       = "${var.app_name}-bucket_reader_queue-${var.env}"
+  name       = "eservice_registry_queue"
   fifo_queue = true
+}
+
+module "sqs_polling_queue" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "4.0.1"
+
+  name = "eservice_polling_queue"
+
+}
+
+
+module "sqs_polling_result_queue" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "4.0.1"
+
+  name = "eservice_polling_result_queue"
+
+}
+
+
+module "sqs_telemetry_result_queue" {
+  source  = "terraform-aws-modules/sqs/aws"
+  version = "4.0.1"
+
+  name = "eservice_telemetry_result_queue"
+
 }
