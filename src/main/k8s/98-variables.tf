5438edf8e14974a725823da4c86448a99b4f72da
Michele De Simone <michele.desimone@pagopa.it>
2023-03-01 19:08:01 +0100

    feat: manage aws-auth roles and users

--- src/main/k8s/98-variables.tf
+++ src/main/k8s/98-variables.tf
@@ -22,11 +22,22 @@ variable "tags" {
 }
 
 variable "eks_cluster_name" {
-  type = string
+  type        = string
   description = "Name of the EKS cluster"
 }
 
+variable "fargate_profiles_roles_names" {
+  type        = list(string)
+  description = "Names of the Fargate profiles roles"
+}
+
 variable "sso_full_admin_role_name" {
-  type = string
+  type        = string
   description = "Name of the SSO 'FullAdmin' role"
+}
+
+variable "iam_users_read_only" {
+  type = list(string)
+  description = "IAM users to be granted read-only access in the cluster"
+  default = []
 }
---
+++ src/main/k8s/templates/aws-auth-user.tpl
@@ -0,0 +1,7 @@
+- userarn: ${user_arn}
+  username: ${k8s_username}
+  groups:
+%{ for group in k8s_groups ~}
+    - ${group}
+%{ endfor ~}
+
