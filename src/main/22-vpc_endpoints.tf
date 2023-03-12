7b1dfd0dcd2991555c13b8c8e29ab7bb91694ad7
Alessio Creo <alessio.creo@keypartner.com>
2023-03-12 14:14:32 +0100

    refactor: Change VPC Endpoint definition for Timestream

--- src/main/22-vpc_endpoints.tf
+++ src/main/22-vpc_endpoints.tf
@@ -15,28 +15,6 @@ module "sqs_sg" {
 
 }
 
-module "endpoints" {
-  source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-
-  vpc_id             = module.vpc.vpc_id
-  security_group_ids = [module.eks.cluster_security_group_id]
-
-  endpoints = {
-    s3 = {
-      # interface endpoint
-      service = "s3"
-    },
-    sqs = {
-      service             = "sqs"
-      private_dns_enabled = true
-      security_group_ids  = [module.sqs_sg.security_group_id]
-    }
-  }
-
-}
-
-#SEPARATE BECAUSE NOT FOUND WITH MODULE
-
 module "timestream_ingest_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
@@ -52,16 +30,6 @@ module "timestream_ingest_sg" {
   ]
   ingress_rules = ["https-443-tcp"]
 
-}
-resource "aws_vpc_endpoint" "timestream_ingest" {
-  vpc_id            = module.vpc.vpc_id
-  service_name      = "com.amazonaws.${var.aws_region}.timestream.ingest-cell1"
-  vpc_endpoint_type = "Interface"
-
-  security_group_ids = [module.timestream_ingest_sg.security_group_id]
-
-  private_dns_enabled = true
-  subnet_ids          = data.aws_subnets.workload.ids
 }
 
 
@@ -81,14 +49,33 @@ module "timestream_query_sg" {
   ingress_rules = ["https-443-tcp"]
 
 }
-resource "aws_vpc_endpoint" "timestream_query" {
-  vpc_id            = module.vpc.vpc_id
-  service_name      = "com.amazonaws.${var.aws_region}.timestream.query-cell1"
-  vpc_endpoint_type = "Interface"
 
-  security_group_ids = [module.timestream_query_sg.security_group_id]
+module "endpoints" {
+  source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
 
-  private_dns_enabled = true
-  subnet_ids          = data.aws_subnets.workload.ids
+  vpc_id             = module.vpc.vpc_id
+  security_group_ids = [module.eks.cluster_security_group_id]
+
+  endpoints = {
+    s3 = {
+      # interface endpoint
+      service = "s3"
+    },
+    sqs = {
+      service             = "sqs"
+      private_dns_enabled = true
+      security_group_ids  = [module.sqs_sg.security_group_id]
+    },
+    timestream_ingest = {
+      service_name        = "com.amazonaws.${var.aws_region}.timestream.ingest-cell1"
+      private_dns_enabled = true
+      security_group_ids  = [module.timestream_ingest_sg.security_group_id]
+    },
+    timestream_query = {
+      service_name        = "com.amazonaws.${var.aws_region}.timestream.query-cell1"
+      private_dns_enabled = true
+      security_group_ids  = [module.timestream_query_sg.security_group_id]
+    }
+  }
+
 }
-
