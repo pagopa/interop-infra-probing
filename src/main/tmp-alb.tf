35b9c69cc9d078b28a33f1b2973b31206e1b4491
Alessio Creo <alessio.creo@keypartner.com>
2023-03-29 10:10:43 +0200

    test: Add test ALB

---
+++ src/main/tmp-alb.tf
@@ -0,0 +1,20 @@
+resource "aws_lb" "alb_eks" {
+  name               = "${var.app_name}-alb-${var.env}"
+  internal           = true
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.workload.ids
+
+  enable_deletion_protection       = true
+  enable_cross_zone_load_balancing = true
+}
+
+
+resource "aws_lb_listener" "alb_eks" {
+  load_balancer_arn = aws_lb.alb_eks.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+  }
+}
