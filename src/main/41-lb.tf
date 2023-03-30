6af8b64f0ac3abda18c8dc5e625ec07e10f7437e
Alessio Creo <alessio.creo@keypartner.com>
2023-03-30 18:02:59 +0200

    fix: Fix tG definition

--- src/main/41-lb.tf
+++ src/main/41-lb.tf
@@ -8,7 +8,27 @@ resource "aws_lb" "nlb" {
   enable_cross_zone_load_balancing = true
 }
 
-resource "aws_lb_listener" "front_end" {
+
+resource "aws_lb_target_group" "alb" {
+  name        = "${var.app_name}-alb-tg-${var.env}"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+
+
+  health_check {
+    healthy_threshold   = 2
+    interval            = 30
+    matcher             = "200-499"
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+  vpc_id = module.vpc.vpc_id
+}
+resource "aws_lb_listener" "nlb_to_alb" {
   load_balancer_arn = aws_lb.nlb.arn
   port              = "80"
   protocol          = "HTTP"
@@ -19,13 +39,22 @@ resource "aws_lb_listener" "front_end" {
   }
 }
 
-resource "aws_lb_target_group" "alb" {
-  name        = "${var.app_name}-alb-tg-${var.env}"
-  target_type = "alb"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = module.vpc.vpc_id
+resource "aws_lb_listener_rule" "nlb_to_alb" {
+  listener_arn = aws_lb_listener.nlb_to_alb.arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/"]
+    }
+  }
 }
+
+
 
 resource "aws_lb_target_group_attachment" "alb" {
   target_group_arn = aws_lb_target_group.alb.arn
