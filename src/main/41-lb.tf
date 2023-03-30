1c9242aa818f8e73a88c11050f373d540cefce11
Alessio Creo <alessio.creo@keypartner.com>
2023-03-30 23:17:29 +0200

    refactor

--- src/main/41-lb.tf
+++ src/main/41-lb.tf
@@ -8,6 +8,16 @@ resource "aws_lb" "nlb" {
   enable_cross_zone_load_balancing = true
 }
 
+resource "aws_lb_listener" "nlb_to_alb" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = "80"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb.arn
+  }
+}
 
 resource "aws_lb_target_group" "alb" {
   name        = "${var.app_name}-alb-tg-${var.env}"
@@ -28,16 +38,7 @@ resource "aws_lb_target_group" "alb" {
   }
   vpc_id = module.vpc.vpc_id
 }
-resource "aws_lb_listener" "nlb_to_alb" {
-  load_balancer_arn = aws_lb.nlb.arn
-  port              = "80"
-  protocol          = "TCP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.alb.arn
-  }
-}
 
 
 resource "aws_lb_target_group_attachment" "alb" {
