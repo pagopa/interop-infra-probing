c98f1b936ff4fd72f545833258536654411fd090
Alessio Creo <alessio.creo@keypartner.com>
2023-04-19 12:05:22 +0200

    fix: Modify cache behaviour

--- src/main/31-cloudfront.tf
+++ src/main/31-cloudfront.tf
@@ -77,48 +77,25 @@ module "fe_cdn" {
   }
   ordered_cache_behavior = [
     {
-      path_pattern = "/eservices/*"
-      # target_origin_id         = "apigw"
-      # viewer_protocol_policy   = "redirect-to-https"
-      # cache_policy_id          = data.aws_cloudfront_cache_policy.caching_disabled.id
-      #origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer_except_host_header.id
-      # allowed_methods          = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-      # compress                 = true
-      # use_forwarded_values     = false
-      allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-      cached_methods   = ["GET", "HEAD"]
-      target_origin_id = "apigw"
+      path_pattern             = "/eservices"
+      target_origin_id         = "apigw"
+      viewer_protocol_policy   = "redirect-to-https"
+      cache_policy_id          = data.aws_cloudfront_cache_policy.caching_disabled.id
+      origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer_except_host_header.id
+      allowed_methods          = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+      compress                 = true
+      use_forwarded_values     = false
 
-      default_ttl  = 0
-      min_ttl      = 0
-      max_ttl      = 0
-      compress     = true
-      query_string = true
-
-
-      viewer_protocol_policy = "redirect-to-https"
     },
     {
-      path_pattern = "/producers/*"
-      # target_origin_id         = "apigw"
-      # viewer_protocol_policy   = "redirect-to-https"
-      # cache_policy_id          = data.aws_cloudfront_cache_policy.caching_disabled.id
-      #origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer_except_host_header.id
-      # allowed_methods          = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-      # compress                 = true
-      # use_forwarded_values     = false
-      allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-      cached_methods   = ["GET", "HEAD"]
-      target_origin_id = "apigw"
-
-      default_ttl  = 0
-      min_ttl      = 0
-      max_ttl      = 0
-      compress     = true
-      query_string = true
-
-
-      viewer_protocol_policy = "redirect-to-https"
+      path_pattern             = "/producers"
+      target_origin_id         = "apigw"
+      viewer_protocol_policy   = "redirect-to-https"
+      cache_policy_id          = data.aws_cloudfront_cache_policy.caching_disabled.id
+      origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer_except_host_header.id
+      allowed_methods          = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+      compress                 = true
+      use_forwarded_values     = false
     }
   ]
 }
