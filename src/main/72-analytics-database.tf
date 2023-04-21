9933d6cef53d13f29b130e1f7402b2e4feb561e1
Alessio Creo <alessio.creo@keypartner.com>
2023-04-21 15:22:32 +0200

    feat: Add timestream table

--- src/main/72-analytics-database.tf
+++ src/main/72-analytics-database.tf
@@ -1,3 +1,8 @@
 resource "aws_timestreamwrite_database" "analytics_database" {
   database_name = var.analytics_database_name
 }
+
+resource "aws_timestreamwrite_table" "polling_measurements" {
+  database_name = aws_timestreamwrite_database.analytics_database.database_name
+  table_name    = "polling_measurements"
+}
