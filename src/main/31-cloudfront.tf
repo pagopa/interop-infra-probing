8893f425bce90e75891d285543cbb6e028158d12
alessio-creo <124075593+alessio-creo@users.noreply.github.com>
2023-04-21 11:38:44 +0200

    fix: CloudFront path patterns (#34)

--- src/main/31-cloudfront.tf
+++ src/main/31-cloudfront.tf
@@ -10,8 +10,8 @@ data "aws_cloudfront_cache_policy" "caching_optimized" {
   name = "Managed-CachingOptimized"
 }
 
-data "aws_cloudfront_origin_request_policy" "all_viewer" {
-  name = "Managed-AllViewer"
+data "aws_cloudfront_origin_request_policy" "all_viewer_except_host_header" {
+  name = "Managed-AllViewerExceptHostHeader"
 }
 
 module "fe_cdn" {
@@ -72,21 +72,22 @@ module "fe_cdn" {
   }
   ordered_cache_behavior = [
     {
-      path_pattern             = "/eservices/*"
+      path_pattern             = "/eservices"
       target_origin_id         = "apigw"
       viewer_protocol_policy   = "redirect-to-https"
       cache_policy_id          = data.aws_cloudfront_cache_policy.caching_disabled.id
-      origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer.id
+      origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer_except_host_header.id
       allowed_methods          = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
       compress                 = true
       use_forwarded_values     = false
+
     },
     {
-      path_pattern             = "/producers/*"
+      path_pattern             = "/producers"
       target_origin_id         = "apigw"
       viewer_protocol_policy   = "redirect-to-https"
       cache_policy_id          = data.aws_cloudfront_cache_policy.caching_disabled.id
-      origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer.id
+      origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer_except_host_header.id
       allowed_methods          = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
       compress                 = true
       use_forwarded_values     = false
