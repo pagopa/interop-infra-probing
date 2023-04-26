2248cbe7f923e568798168c20ef54ea501b70730
alessio-creo <124075593+alessio-creo@users.noreply.github.com>
2023-04-26 10:53:09 +0200

    Update src/main/72-analytics-database.tf
    Co-authored-by: Michele De Simone <106953981+micdes-pagopa@users.noreply.github.com>

--- src/main/72-analytics-database.tf
+++ src/main/72-analytics-database.tf
@@ -2,7 +2,7 @@ resource "aws_timestreamwrite_database" "analytics_database" {
   database_name = var.analytics_database_name
 }
 
-resource "aws_timestreamwrite_table" "polling_measurements" {
+resource "aws_timestreamwrite_table" "probing_telemetry" {
   database_name = aws_timestreamwrite_database.analytics_database.database_name
   table_name    = "polling_measurements"
 }
