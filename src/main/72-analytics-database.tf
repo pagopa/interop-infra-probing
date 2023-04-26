2a907b5d7cf0b9020fd66a54950cb389213e1583
Alessio Creo <alessio.creo@keypartner.com>
2023-04-26 14:37:33 +0200

    fix: Add retention properties to timestream table

--- src/main/72-analytics-database.tf
+++ src/main/72-analytics-database.tf
@@ -5,4 +5,8 @@ resource "aws_timestreamwrite_database" "analytics_database" {
 resource "aws_timestreamwrite_table" "probing_telemetry" {
   database_name = aws_timestreamwrite_database.analytics_database.database_name
   table_name    = "probing_telemetry"
+  retention_properties {
+    magnetic_store_retention_period_in_days = var.timestream_table_magnetic_store_retention_period_in_days
+    memory_store_retention_period_in_hours  = var.timestream_table_memory_store_retention_period_in_hours
+  }
 }
