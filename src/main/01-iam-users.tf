41fa36ee2ff4b511557b7c32693a0c7b68cc9bbc
Alessio Creo <alessio.creo@keypartner.com>
2023-05-02 16:42:38 +0200

    fix: policy formatting

--- src/main/01-iam-users.tf
+++ src/main/01-iam-users.tf
@@ -213,9 +213,9 @@ resource "aws_iam_group_policy" "timestream_development" {
         ]
         Resource = [
 
-          module.sqs_telemetry_result_queue.queue_arn
+          "${module.sqs_telemetry_result_queue.queue_arn}"
         ]
-      },
+      }
 
     ]
   })
