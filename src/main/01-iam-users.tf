b34c924751b29c83b6072af704f45967f160c04b
Alessio Creo <alessio.creo@keypartner.com>
2023-05-16 15:41:46 +0200

    refactor: Add read-write-delete message permission to developers

--- src/main/01-iam-users.tf
+++ src/main/01-iam-users.tf
@@ -213,6 +213,9 @@ resource "aws_iam_group_policy" "timestream_development" {
         ]
         Resource = [
 
+          "${module.sqs_registry_queue.queue_arn}",
+          "${module.sqs_polling_queue.queue_arn}",
+          "${module.sqs_polling_result_queue.queue_arn}",
           "${module.sqs_telemetry_result_queue.queue_arn}"
         ]
       }
