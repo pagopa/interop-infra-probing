272a7a22ed3182ee4b39f87e938ec05dad2e63a5
Alessio Creo <alessio.creo@keypartner.com>
2023-05-18 15:41:26 +0200

    feat: Init user pool

---
+++ src/main/12-cognito.tf
@@ -0,0 +1,11 @@
+resource "aws_cognito_user_pool" "user_pool" {
+  name                = "${var.app_name}-user-pool-${var.env}"
+  deletion_protection = "ACTIVE"
+  mfa_configuration   = "OFF"
+
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_message        = "{####} Insert html"
+    email_subject        = "Confirmation subject"
+  }
+}
