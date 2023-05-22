b741aa0c97e480da5403c91a8bad7d5e9b30cd33
Alessio Creo <alessio.creo@keypartner.com>
2023-05-22 15:12:35 +0200

    fix: Align cognito

--- src/main/12-cognito.tf
+++ src/main/12-cognito.tf
@@ -3,6 +3,16 @@ resource "aws_cognito_user_pool" "user_pool" {
   deletion_protection = "ACTIVE"
   mfa_configuration   = "OFF"
 
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+    recovery_mechanism {
+      name     = "verified_phone_number"
+      priority = 2
+    }
+  }
   verification_message_template {
     default_email_option = "CONFIRM_WITH_CODE"
     email_message        = "{####} Insert html"
