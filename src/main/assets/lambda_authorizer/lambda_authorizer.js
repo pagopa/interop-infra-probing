c61b651e1f88574df37efc250124c9c25722acdd
Alessio Creo <alessio.creo@keypartner.com>
2023-05-24 10:42:06 +0200

    feat: Add lambda authorizer

--- src/main/assets/lambda_authorizer/lambda_authorizer.js
+++ src/main/assets/lambda_authorizer/lambda_authorizer.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const authMapping = JSON.parse(process.env.ROLE_MAPPING)
 
 export const handler =  function(event, context, callback) {
@@ -9,11 +10,25 @@ export const handler =  function(event, context, callback) {
     console.log("Parsing JSON payload")
     var payload = JSON.parse(decodedPayload);
 
+=======
+import authMapping from './mapping.json' assert { type: 'json' };
+
+export const handler =  function(event, context, callback) {
+    console.log("Getting payload")
+    var encodedPayload = event.headers.Authorization.split(' ')[1].split('.')[1];
+    console.log("Decoding payload")
+    encodedPayload = atob(encodedPayload);
+    console.log("Parsing JSON payload")
+    var payload = JSON.parse(encodedPayload);
+    console.log(payload);
+    
+>>>>>>> 407cc81 (feat: Add lambda authorizer)
     const method = event.httpMethod;
     const resource = event.resource;
     const groups = payload["cognito:groups"];
     
     console.log("Checking authorization")
+<<<<<<< HEAD
     var isAuthorized = groups.some( (x) => getAuthorization(x,resource,method) )
     
     console.log("Generating authorization policy")
@@ -24,6 +39,25 @@ export const handler =  function(event, context, callback) {
     } else {
         callback(null, generatePolicy('Deny', event.methodArn));
         console.log("User NOT allowed to perform the API call")
+=======
+    for (const group of groups) {
+        console.log(group)
+        var isAuthorized = getAuthorization(group,resource,method)
+        if (isAuthorized) {
+            break;
+        }
+    }
+    console.log("Generating authorization policy")
+    switch(isAuthorized) {
+        case true:
+            callback(null, generatePolicy( 'Allow', event.methodArn));
+            break;
+        case false:
+            callback(null, generatePolicy('Deny', event.methodArn));
+            break;
+        default:
+            callback("Unauthorized");
+>>>>>>> 407cc81 (feat: Add lambda authorizer)
     }
         
 };
@@ -39,6 +73,7 @@ function getAuthorization(group,resource,method) {
 
 
 var generatePolicy = function(effect, resource) {
+<<<<<<< HEAD
 
     
         var policyDocument = {
@@ -55,6 +90,21 @@ var generatePolicy = function(effect, resource) {
     var authResponse = {
         "policyDocument":policyDocument
     };
+=======
+    var authResponse = {};
+    
+    if (effect && resource) {
+        var policyDocument = {};
+        policyDocument.Version = '2012-10-17'; 
+        policyDocument.Statement = [];
+        var statementOne = {};
+        statementOne.Action = 'execute-api:Invoke'; 
+        statementOne.Effect = effect;
+        statementOne.Resource = resource;
+        policyDocument.Statement[0] = statementOne;
+        authResponse.policyDocument = policyDocument;
+    }
+>>>>>>> 407cc81 (feat: Add lambda authorizer)
     
     return authResponse;
 }
