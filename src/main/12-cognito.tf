0183e9b59f5ea6de3fafd7bf6b9ab99851635411
Alessio Creo <alessio.creo@keypartner.com>
2023-06-09 17:19:03 +0200

    fix: remove verification template

--- src/main/12-cognito.tf
+++ src/main/12-cognito.tf
@@ -17,11 +17,6 @@ resource "aws_cognito_user_pool" "user_pool" {
       priority = 2
     }
   }
-  verification_message_template {
-    default_email_option = "CONFIRM_WITH_CODE"
-    email_message        = "{####} Insert html"
-    email_subject        = "Confirmation subject"
-  }
 }
 
 resource "aws_cognito_user_group" "admins" {
