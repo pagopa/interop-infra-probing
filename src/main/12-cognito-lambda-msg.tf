3648c35a44efbd1921aeaecba6401b5140781ad0
Alessio Creo <alessio.creo@keypartner.com>
2023-06-09 11:24:13 +0200

    fix: Add lambda permission from cognito

--- src/main/12-cognito-lambda-msg.tf
+++ src/main/12-cognito-lambda-msg.tf
@@ -1,3 +1,11 @@
+resource "aws_lambda_permission" "allow_cognito" {
+  statement_id  = "AllowExecutionFromCognito"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.cognito_messaging.function_name
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.user_pool.arn
+}
+
 data "aws_iam_policy_document" "congito_messaging_assume_role" {
   statement {
     effect = "Allow"
