ad0c7d9941e204aefe21aad927f399a130c8757f
Alessio Creo <alessio.creo@keypartner.com>
2023-06-09 16:23:47 +0200

    feat: Add email templating

--- src/main/assets/lambda_cognito_messaging/lambda_cognito_messaging.js
+++ src/main/assets/lambda_cognito_messaging/lambda_cognito_messaging.js
@@ -1,8 +1,9 @@
+import _ from 'lodash/template';
 exports.handler = async (event) => {
     if (event.triggerSource === "CustomMessage_ForgotPassword") {
-      const message = `Your code is ${event.request.codeParameter}.`;
+      const message = _.template(process.env.RESET_PASSWORD_HTML_CONTENT, {fe_url: process.env.FE_URL, reset_password_route: process.env.RESET_PASSOWORD_ROUTE, reset_code: event.request.codeParameter,username:event.request.usernameParameter});
       event.response.emailMessage = message;
-      event.response.emailSubject = "Reset Password";
+      event.response.emailSubject = "Ripristino Password";
     } else   if (event.triggerSource === "CustomMessage_AdminCreateUser") {
       const message = `Welcome to the service. Your user name is ${event.request.usernameParameter}. Your temporary password is ${event.request.codeParameter}`;
       event.response.smsMessage = message;
