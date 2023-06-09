719ca3cbc92d7dceb3f3f95ba8ba0f9b7a6de641
Alessio Creo <alessio.creo@keypartner.com>
2023-06-09 12:43:59 +0200

    test: Custom messaging

--- src/main/assets/lambda_cognito_messaging/lambda_cognito_messaging.js
+++ src/main/assets/lambda_cognito_messaging/lambda_cognito_messaging.js
@@ -3,6 +3,11 @@ const handler = async (event) => {
       const message = `Your code is ${event.request.codeParameter}.`;
       event.response.emailMessage = message;
       event.response.emailSubject = "Reset Password";
+    } else   if (event.triggerSource === "CustomMessage_AdminCreateUser") {
+      const message = `Welcome to the service. Your user name is ${event.request.usernameParameter}. Your temporary password is ${event.request.codeParameter}`;
+      event.response.smsMessage = message;
+      event.response.emailMessage = message;
+      event.response.emailSubject = "Welcome to the service";
     }
     return event;
   };
