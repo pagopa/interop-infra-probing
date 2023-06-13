55678eb94d1118f58a7926851a018a94f33d9b0b
Alessio Creo <alessio.creo@keypartner.com>
2023-06-13 16:42:18 +0200

    refactor: Change email behaviour

--- src/main/assets/lambda_cognito_messaging/lambda_cognito_messaging.js
+++ src/main/assets/lambda_cognito_messaging/lambda_cognito_messaging.js
@@ -2,14 +2,13 @@ const _ = require('lodash.template');
 const fs = require('fs')
 exports.handler = async (event) => {
     if (event.triggerSource === "CustomMessage_ForgotPassword") {
-      const message = _(fs.readFileSync('email_templates/resetPassword.html'), {fe_url: process.env.FE_URL, reset_password_route: process.env.RESET_PASSOWORD_ROUTE, reset_code: event.request.codeParameter,username:event.request.usernameParameter});
+      const message = _(fs.readFileSync('email_templates/forgotPasswordMailTemplate.html'), {fe_url: process.env.FE_URL, reset_password_route: process.env.RESET_PASSOWORD_ROUTE, reset_code: event.request.codeParameter,username:event.request.usernameParameter});
       event.response.emailMessage = message;
       event.response.emailSubject = "Ripristino Password";
     } else   if (event.triggerSource === "CustomMessage_AdminCreateUser") {
-      const message = `Welcome to the service. Your user name is ${event.request.usernameParameter}. Your temporary password is ${event.request.codeParameter}`;
-      event.response.smsMessage = message;
+      const message = _(fs.readFileSync('email_templates/forgotPasswordMailTemplate.html'), {fe_url: process.env.FE_URL, reset_password_route: process.env.LOGIN_ROUTE);;
       event.response.emailMessage = message;
-      event.response.emailSubject = "Welcome to the service";
+      event.response.emailSubject = "Nuova utenza attiva";
     }
     return event;
 };  
