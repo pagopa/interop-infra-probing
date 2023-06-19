a504221550a02cfbe780c151e375ae3890bc0c7a
Alessio Creo <alessio.creo@keypartner.com>
2023-06-19 12:13:19 +0200

    feat: Add ADOT monitoring

--- src/main/61-eks-role.tf
+++ src/main/61-eks-role.tf
@@ -211,6 +211,23 @@ module "aws_load_balancer_controller_role" {
   role_description = "Role for AWS Load Balancer Controller"
 
   role_policy_arns = {
-    registry_reader_policy = aws_iam_policy.aws_load_balancer_controller_iam_policy.arn
+    aws_load_balancer_controller_policy = aws_iam_policy.aws_load_balancer_controller_iam_policy.arn
+  }
+}
+
+module "adot_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name = "adot"
+
+  role_policy_arns = {
+    cloud_watch = data.aws_iam_policy.aws_managed_cloudwatch_agent_server.arn
+  }
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["aws-observability:aws-otel-collector"]
+    }
   }
 }
