82455f98b719968ef1654204f7050f2e69120f57
Alessio Creo <alessio.creo@keypartner.com>
2023-06-23 10:22:27 +0200

    test: remove adot addon

--- src/main/k8s/26-metrics-server.tf
+++ src/main/k8s/26-metrics-server.tf
@@ -1,7 +1,7 @@
 resource "kubernetes_manifest" "metrics_server" {
   for_each = fileset("${path.module}/assets/metrics_server/", "*.yaml")
 
-  manifest = yamldecode(templatefile("${path.module}/assets/metrics_server/${each.value}",{metrics_server_img_tag=var.metrics_server_img_tag}))
+  manifest = yamldecode(templatefile("${path.module}/assets/metrics_server/${each.value}", { metrics_server_img_tag = var.metrics_server_img_tag }))
   field_manager {
     force_conflicts = true
   }
