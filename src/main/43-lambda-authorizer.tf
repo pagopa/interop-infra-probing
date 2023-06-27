e5421234e2e8b3e8dc48748031d3bcbdd5d52863
alessio-creo <124075593+alessio-creo@users.noreply.github.com>
2023-06-27 10:32:49 +0200

    Update src/main/43-lambda-authorizer.tf
    Co-authored-by: Michele De Simone <106953981+micdes-pagopa@users.noreply.github.com>

--- src/main/43-lambda-authorizer.tf
+++ src/main/43-lambda-authorizer.tf
@@ -104,7 +104,7 @@ resource "aws_lambda_function" "cognito_authorizer" {
       ENV           = var.env
       ROLE_MAPPING  = data.local_file.role_mapping.content
       JWKS_URI      = "https://cognito-idp.${var.aws_region}.amazonaws.com/${aws_cognito_user_pool.user_pool.id}/.well-known/jwks.json"
-      CACHE         = var.lambda_authorizer_cache_enabled
+      JWKS_CACHE_ENABLED         = var.lambda_authorizer_cache_enabled
       CACHE_MAX_AGE = var.lambda_authorizer_cache_max_age
     }
   }
