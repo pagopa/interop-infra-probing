ccab94a93bfc81a96f54ff81a1f8ac797360d9ab
Alessio Creo <alessio.creo@keypartner.com>
2023-06-28 17:21:58 +0200

    refactor: change alarms split and naming

--- src/main/16-cloudwatch-alarms.tf
+++ src/main/16-cloudwatch-alarms.tf
@@ -1,7 +1,75 @@
+locals {
+  microservices = ["interop-probing-api", "interop-probing-caller", "interop-probing-eservice-operations",
+    "interop-probing-eservice-registry-reader", "interop-probing-eservice-registry-updater",
+  "interop-probing-response-updater", "interop-probing-scheduler", "interop-probing-telemetry-writer", "interop-probing-statistics-api"]
+
+  lambda_functions = [
+    aws_lambda_function.cognito_authorizer.function_name,
+    aws_lambda_function.external_authorizer.function_name,
+    aws_lambda_function.cognito_messaging.function_name,
+  ]
+  sqs_queues = [
+    module.sqs_registry_queue.queue_name,
+    module.sqs_polling_queue.queue_name,
+    module.sqs_polling_result_queue.queue_name,
+    module.sqs_telemetry_result_queue.queue_name
+  ]
+
+}
+
+resource "aws_cloudwatch_metric_alarm" "sqs_message_age" {
+  for_each            = toset(local.sqs_queues)
+  treat_missing_data  = "notBreaching"
+  alarm_name          = "${var.app_name}-sqs-message-age-${each.value}-${var.env}"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 10
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = "AWS/SQS"
+  period              = 60
+  statistic           = "Average"
+  dimensions = {
+    QueueName = each.value
+  }
+  threshold     = var.cw_alarm_thresholds.sqs_message_age
+  alarm_actions = [aws_sns_topic.cw_alarms.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
+  for_each            = toset(local.lambda_functions)
+  treat_missing_data  = "notBreaching"
+  alarm_name          = "${var.app_name}-lambda-errors-${each.value}-${var.env}"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 10
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Sum"
+
+  dimensions = {
+    FunctionName = each.value
+  }
+  threshold     = 1
+  alarm_actions = [aws_sns_topic.cw_alarms.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_concurrency_pct" {
+  for_each            = toset(local.lambda_functions)
+  treat_missing_data  = "notBreaching"
+  alarm_name          = "${var.app_name}-lambda-conc-util-pct-${each.value}-${var.env}"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "ProvisionedConcurrencyUtilization"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Maximum"
+  threshold           = var.cw_alarm_thresholds.lambda_concurrency_utilization
+  alarm_actions       = [aws_sns_topic.cw_alarms.arn]
+}
 
 
 resource "aws_cloudwatch_metric_alarm" "apigw_server_errors" {
   alarm_name          = "${var.app_name}-apigw-5xx-${var.env}"
+  treat_missing_data  = "notBreaching"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 10
   metric_name         = "5XXError"
@@ -30,7 +98,7 @@ resource "aws_cloudwatch_log_metric_filter" "error_logs" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "error_logs" {
-  alarm_name          = "${var.app_name}-cwalarm-application-errors-${var.env}"
+  alarm_name          = "${var.app_name}-application-errors-${var.env}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 10
   metric_name         = "ErrorCount"
@@ -42,44 +110,10 @@ resource "aws_cloudwatch_metric_alarm" "error_logs" {
 }
 
 
-resource "aws_cloudwatch_metric_alarm" "sqs_message_age" {
-  alarm_name          = "${var.app_name}-cwalarm-sqs-message-age-${var.env}"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 10
-  metric_name         = "ApproximateAgeOfOldestMessage"
-  namespace           = "AWS/SQS"
-  period              = 60
-  statistic           = "Average"
-  threshold           = var.cw_alarm_thresholds.sqs_message_age
-  alarm_actions       = [aws_sns_topic.cw_alarms.arn]
-}
 
-resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
-  alarm_name          = "${var.app_name}-cwalarm-lambda-errors-${var.env}"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 10
-  metric_name         = "Errors"
-  namespace           = "AWS/Lambda"
-  period              = 60
-  statistic           = "Sum"
-  threshold           = 1
-  alarm_actions       = [aws_sns_topic.cw_alarms.arn]
-}
-
-resource "aws_cloudwatch_metric_alarm" "lambda_concurrency_pct" {
-  alarm_name          = "${var.app_name}-cwalarm-lambda-concurrency-utilization-pct-${var.env}"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 1
-  metric_name         = "ProvisionedConcurrencyUtilization"
-  namespace           = "AWS/Lambda"
-  period              = 60
-  statistic           = "Maximum"
-  threshold           = var.cw_alarm_thresholds.lambda_concurrency_utilization
-  alarm_actions       = [aws_sns_topic.cw_alarms.arn]
-}
 
 resource "aws_cloudwatch_metric_alarm" "timestream_errors" {
-  alarm_name          = "${var.app_name}-cwalarm-timestream-system-errors-${var.env}"
+  alarm_name          = "${var.app_name}-timestream-system-errors-${var.env}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 10
   metric_name         = "SystemErrors"
