e2d216458b8aeba96ff29bcfb062c8b9c14dfd6c
Michele De Simone <michele.desimone@pagopa.it>
2023-07-10 09:52:56 +0200

    feat: SES events SNS topic

--- src/main/15-sns-topic.tf
+++ src/main/15-sns-topic.tf
@@ -34,10 +34,35 @@ data "aws_iam_policy_document" "cw_alarms_publish" {
   }
 }
 
-
-
 resource "aws_sns_topic_policy" "cw_alarms_publish" {
   arn = aws_sns_topic.cw_alarms.arn
 
   policy = data.aws_iam_policy_document.cw_alarms_publish.json
+}
+
+resource "aws_sns_topic" "ses_events" {
+  name = "${var.app_name}-ses-events-${var.env}"
+}
+
+resource "aws_sns_topic_policy" "ses_publish" {
+  arn = aws_sns_topic.ses_events.arn
+
+  policy = jsonencode({
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ses.amazonaws.com"
+        },
+        Action   = "sns:Publish",
+        Resource = aws_sns_topic.ses_events.arn
+        Condition = {
+          StringEquals = {
+            "AWS:SourceAccount" : data.aws_caller_identity.current.account_id,
+            "AWS:SourceArn" : aws_sesv2_email_identity.stato_eservice.arn
+          }
+        }
+      }
+    ]
+  })
 }
