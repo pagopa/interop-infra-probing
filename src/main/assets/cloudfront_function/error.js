3e1256f587303821e40ba39a264dd1d87e0253bb
alessio-creo <124075593+alessio-creo@users.noreply.github.com>
2023-08-03 09:35:35 +0200

    Update src/main/assets/cloudfront_function/error.js
    Co-authored-by: Michele De Simone <106953981+micdes-pagopa@users.noreply.github.com>

--- src/main/assets/cloudfront_function/error.js
+++ src/main/assets/cloudfront_function/error.js
@@ -1,6 +1,8 @@
 function handler(event) {
     var request = event.request;
-    
+          if (request.uri.endsWith('/')) {
+          request.uri += 'index.html';
+      }
     if (!request.uri.includes('.'))
         request.uri = '/index.html';
 
