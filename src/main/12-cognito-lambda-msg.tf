7d78c0862dd8a423ebd8681671aa098dc5e16c4a
Alessio Creo <alessio.creo@keypartner.com>
2023-08-11 11:39:35 +0200

    test: change deploy for lambda messaging

--- src/main/12-cognito-lambda-msg.tf
+++ src/main/12-cognito-lambda-msg.tf
@@ -47,9 +47,8 @@ resource "aws_s3_object" "lambda_cognito_messaging" {
 
 
 resource "aws_lambda_function" "cognito_messaging" {
-  # s3_bucket = module.lambda_packages_bucket.s3_bucket_id
-  # s3_key = "lambda_cognito_messaging.zip"
-  filename      = "${path.module}/assets/lambda_cognito_messaging/lambda_cognito_messaging.zip"
+  s3_bucket     = module.lambda_packages_bucket.s3_bucket_id
+  s3_key        = "lambda_cognito_messaging.zip"
   function_name = "${var.app_name}-lambda-cognito-messaging-${var.env}"
   role          = aws_iam_role.lambda_cognito_messaging_execution_role.arn
   handler       = "index.handler"
