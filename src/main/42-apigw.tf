7bd65ef31630117b1b26f27febbcf2b8b53ad65d
Alessio Creo <alessio.creo@keypartner.com>
2023-08-31 18:01:47 +0200

    format

--- src/main/42-apigw.tf
+++ src/main/42-apigw.tf
@@ -10,7 +10,7 @@ resource "aws_api_gateway_rest_api" "apigw" {
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.apigw.id
-  depends_on = [ aws_api_gateway_integration_response.response_404 ]
+  depends_on  = [aws_api_gateway_integration_response.response_404]
   triggers = {
     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.apigw.body))
   }
