7ac668d6cd93b3ee01de0c210e56490f700a2689
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2025-06-24 16:58:29 +0200

    fix(network): move MSK VPC connection to core-aws layer, remove RDS, Timestream and MSK VPC Endpoints (#83)

--- src/main-refactor/network/98-variables.tf
+++ src/main-refactor/network/98-variables.tf
@@ -31,12 +31,6 @@ variable "sso_admin_role_name" {
   description = "Name of the SSO admin role"
 }
 
-variable "interop_msk_cluster_arn" {
-  description = "ARN of the Interop MSK cluster"
-  type        = string
-  default     = null
-}
-
 variable "dns_probing_base_domain" {
   description = "Base DNS domain for the Probing product"
   type        = string
