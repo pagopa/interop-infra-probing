dda7da9ee91418f4589084a9d8d78e5e002c4ff8
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2025-06-24 15:50:37 +0200

    remove MSK VPC Endpoint

--- src/main-refactor/network/20-vpc-endpoints.tf
+++ src/main-refactor/network/20-vpc-endpoints.tf
@@ -107,15 +107,6 @@ module "vpce_interface" {
       private_dns_enabled = true
 
       tags = { Name = "CloudWatch-Logs" }
-    },
-    # Set 'private_dns_enabled' to 'false' when creating it for the first time, then wait until the service owner accepts your connection request before setting it to true. 
-    # Check the status of this endpoint connection in the AWS Console: it must transition from 'Pending acceptance' to 'Available' before setting private_dns_enabled to true.
-    msk = {
-      service_name        = "com.amazonaws.${var.aws_region}.kafka"
-      service_type        = "Interface"
-      private_dns_enabled = false
-
-      tags = { Name = "MSK" }
     }
   }
 }
