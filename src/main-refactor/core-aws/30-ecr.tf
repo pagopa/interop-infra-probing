3ca03e2f95aff3e1dec762197f1628477695c43a
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2025-06-25 14:51:14 +0200

    add ECR repos and policies

--- src/main-refactor/core-aws/30-ecr.tf
+++ src/main-refactor/core-aws/30-ecr.tf
@@ -1,73 +1,78 @@
-# locals {
-#   repository_names = [
-#     "tracing-be-api",
-#   ]
-# }
+locals {
+  repository_names = [
+    "probing-be-api",
+    "probing-be-scheduler",
+    "probing-be-telemetry-writer",
+    "probing-be-caller",
+    "probing-be-response-updater",
+    "probing-be-statistics-api"
+  ]
+}
 
-# resource "aws_ecr_repository" "app" {
-#   for_each = toset(local.repository_names)
+resource "aws_ecr_repository" "app" {
+  for_each = var.env == "prod" ? toset(local.repository_names) : []
 
-#   image_tag_mutability = "MUTABLE" # needed to override "latest" tag
-#   name                 = each.key
-# }
+  image_tag_mutability = "MUTABLE" # needed to override "latest" tag
+  name                 = each.key
+}
 
-# resource "aws_ecr_lifecycle_policy" "delete_untagged" {
-#   for_each = aws_ecr_repository.app
+resource "aws_ecr_lifecycle_policy" "delete_untagged" {
+  for_each = var.env == "prod" ? aws_ecr_repository.app : {}
 
-#   repository = each.value.name
-#   policy     = <<EOF
-#   {
-#     "rules": [
-#       {
-#         "rulePriority": 1,
-#         "description": "Delete untagged images",
-#         "selection": {
-#           "tagStatus": "untagged",
-#           "countType": "sinceImagePushed",
-#           "countUnit": "days",
-#           "countNumber": 31
-#         },
-#         "action": {
-#           "type": "expire"
-#         }
-#       }
-#     ]
-#   }
-#   EOF
-# }
+  repository = each.value.name
+  policy     = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Delete untagged images",
+        "selection": {
+          "tagStatus": "untagged",
+          "countType": "sinceImagePushed",
+          "countUnit": "days",
+          "countNumber": 31
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+  EOF
+}
 
-# resource "aws_ecr_repository_policy" "cross_account_pull" {
-#   for_each = var.env == "prod" ? aws_ecr_repository.app : {}
+resource "aws_ecr_repository_policy" "cross_account_pull" {
+  for_each = var.env == "prod" ? aws_ecr_repository.app : {}
 
-#   repository = each.value.name
+  repository = each.value.name
 
-#   policy = jsonencode({
-#     Version = "2008-10-17",
-#     Statement = [
-#       {
-#         Sid    = "DEV Image Pull",
-#         Effect = "Allow",
-#         Principal = {
-#           AWS = "arn:aws:iam::774300547186:root"
-#         },
-#         Action = [
-#           "ecr:BatchCheckLayerAvailability",
-#           "ecr:BatchGetImage",
-#           "ecr:GetDownloadUrlForLayer"
-#         ]
-#       },
-#       {
-#         Sid    = "UAT Image Pull",
-#         Effect = "Allow",
-#         Principal = {
-#           AWS = "arn:aws:iam::010158505074:root"
-#         },
-#         Action = [
-#           "ecr:BatchCheckLayerAvailability",
-#           "ecr:BatchGetImage",
-#           "ecr:GetDownloadUrlForLayer"
-#         ]
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2008-10-17",
+    Statement = [
+      {
+        Sid    = "DEV Image Pull",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::774300547186:root"
+        },
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer"
+        ]
+      },
+      {
+        Sid    = "UAT Image Pull",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::010158505074:root"
+        },
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer"
+        ]
+      }
+    ]
+  })
+}
