1e9bc61b39ccb7ce82ffe17a8a35860dec76f723
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2025-07-18 18:46:19 +0200

    remove comment

--- src/main-refactor/stages/modules/influx-user/10-secretsmanager.tf
+++ src/main-refactor/stages/modules/influx-user/10-secretsmanager.tf
@@ -1,11 +1,3 @@
-# resource "random_password" "this" {
-#   length  = var.generated_password_length
-#   special = false
-#   upper   = true
-#   lower   = true
-#   numeric = true
-# }
-
 data "aws_secretsmanager_random_password" "this" {
   password_length            = var.generated_password_length
   require_each_included_type = true
