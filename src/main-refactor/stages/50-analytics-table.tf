879d585febc5e3b00777f872e6ecd65fa764c39c
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2025-07-21 11:02:24 +0200

    remove old analytics resources

--- src/main-refactor/stages/50-analytics-table.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_timestreamwrite_table" "probing_telemetry" {
-#   database_name = data.aws_timestreamwrite_database.probing_analytics_database.database_name
-#   table_name    = format("probing_telemetry_%s", var.stage)
-
-#   retention_properties {
-#     magnetic_store_retention_period_in_days = var.timestream_table_magnetic_store_retention_period_in_days
-#     memory_store_retention_period_in_hours  = var.timestream_table_memory_store_retention_period_in_hours
-#   }
-# }
