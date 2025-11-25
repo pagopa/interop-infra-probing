945eb5689a8761702e6ff1a5a27035fdd33c0095
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2025-11-25 15:18:39 +0100

    add permissions to perform s3-sync and create-invalidation to deployment role

---
+++ src/main-refactor/stages/90-iam-github.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_role_policy_attachment" "s3_sync_deployment_github_repo" {
+  role       = data.aws_iam_role.probing_deployment_github_repo_role.name
+  policy_arn = aws_iam_policy.s3_sync_deployment_github_repo.arn
+}
+
+resource "aws_iam_policy" "s3_sync_deployment_github_repo" {
+  depends_on = [module.fe_bucket, module.fe_cdn]
+  name       = "S3SyncAndInvalidateCloudFront"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          module.fe_bucket.s3_bucket_arn,
+          format("%s/*", module.fe_bucket.s3_bucket_arn)
+        ]
+      },
+      {
+        Effect   = "Allow"
+        Action   = "cloudfront:CreateInvalidation"
+        Resource = module.fe_cdn.cloudfront_distribution_arn
+      }
+    ]
+  })
+}
