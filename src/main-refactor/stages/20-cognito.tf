ad5911ad1ead35ffca27fb9e45195219b91554e1
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2026-02-06 10:15:09 +0100

    fix(stages): remove Cognito user resources and json files (#103)

--- src/main-refactor/stages/20-cognito.tf
+++ src/main-refactor/stages/20-cognito.tf
@@ -49,90 +49,9 @@ resource "aws_cognito_user_pool_client" "client" {
 
   user_pool_id    = aws_cognito_user_pool.user_pool.id
   generate_secret = false
-}
 
-locals {
-  cognito_users_json_data = jsondecode(file("./assets/cognito-users/cognito-users-${var.stage}.json"))
-
-  cognito_admins = try([
-    for admin in local.cognito_users_json_data.admins : {
-      username = admin.username
-      email    = admin.email
-    }
-  ], [])
-
-  cognito_users = try([
-    for user in local.cognito_users_json_data.users : {
-      username = user.username
-      email    = user.email
-    }
-  ], [])
-}
-
-data "aws_secretsmanager_random_password" "admins" {
-  for_each = { for admin in local.cognito_admins : "${admin.username}" => admin }
-
-  password_length            = 16
-  require_each_included_type = true
-}
-
-resource "aws_cognito_user" "admins" {
-  for_each = { for admin in local.cognito_admins : "${admin.username}" => admin }
-
-  user_pool_id = aws_cognito_user_pool.user_pool.id
-  username     = each.value.username
-  password     = data.aws_secretsmanager_random_password.admins[each.key].random_password
-
-  attributes = {
-    email          = each.value.email
-    email_verified = true
+  access_token_validity = var.stage == "qa" ? 24 : 1
+  token_validity_units {
+    access_token = "hours"
   }
-
-  force_alias_creation = true
-
-  lifecycle {
-    ignore_changes = [password]
-  }
-}
-
-resource "aws_cognito_user_in_group" "admins" {
-  for_each = aws_cognito_user.admins
-
-  user_pool_id = aws_cognito_user_pool.user_pool.id
-  group_name   = aws_cognito_user_group.admins.name
-  username     = each.value.username
-}
-
-data "aws_secretsmanager_random_password" "users" {
-  for_each = { for user in local.cognito_users : "${user.username}" => user }
-
-  password_length            = 16
-  require_each_included_type = true
-}
-
-resource "aws_cognito_user" "users" {
-  for_each = { for user in local.cognito_users : "${user.username}" => user }
-
-  user_pool_id = aws_cognito_user_pool.user_pool.id
-  username     = each.value.username
-  password     = data.aws_secretsmanager_random_password.users[each.key].random_password
-
-  attributes = {
-    email          = each.value.email
-    email_verified = true
-  }
-
-  force_alias_creation = true
-
-  lifecycle {
-    ignore_changes = [password]
-  }
-}
-
-resource "aws_cognito_user_in_group" "users" {
-  for_each = aws_cognito_user.users
-
-  user_pool_id = aws_cognito_user_pool.user_pool.id
-  group_name   = aws_cognito_user_group.users.name
-  username     = each.value.username
 }
