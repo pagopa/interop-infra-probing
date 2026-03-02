535b8a7844efc0096b115542e777be47b25cec68
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2026-03-02 17:42:51 +0100

    edit local

--- src/main-refactor/stages/00-main.tf
+++ src/main-refactor/stages/00-main.tf
@@ -46,7 +46,7 @@ locals {
   project                        = "probing"
   app_name                       = format("interop-%s", local.project)
   terraform_state                = "stages"
-  use_postgresql_user_module     = var.stage == "dev" || var.stage == "qa"
+  use_postgresql_user_module     = var.stage == "dev" || var.stage == "qa" || var.stage == "uat"
   deploy_interop_msk_integration = var.interop_msk_cluster_arn != null
 }
 
