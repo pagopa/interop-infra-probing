1c108d64c0502af5195f8887ffc54fd3fa7902c3
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2026-03-03 09:23:20 +0100

    add CW logs queries

---
+++ src/main-refactor/stages/80-cloudwatch-logs-queries.tf
@@ -0,0 +1,67 @@
+resource "aws_cloudwatch_query_definition" "app_logs_errors" {
+  name = "Application-Logs-Errors"
+
+  log_group_names = [var.eks_application_log_group_name]
+
+  query_string = <<-EOT
+    fields @timestamp, @message
+    | sort @timestamp asc
+    | filter (@message like /ERROR/ or stream = "stderr")
+    | filter @logStream not like /adot-collector/
+    # | filter pod_app like /probing-be-api/
+    # | filter pod_namespace = "dev"
+  EOT
+}
+
+resource "aws_cloudwatch_query_definition" "cid_tracker" {
+  name = "CID-Tracker"
+
+  log_group_names = [var.eks_application_log_group_name]
+
+  query_string = <<-EOT
+    fields @timestamp, @message
+    | sort @timestamp asc
+    | parse @message "[CID=*]" as CID
+    | filter CID = ""
+    | display @message
+  EOT
+}
+
+resource "aws_cloudwatch_query_definition" "apigw_5xx" {
+  name = "APIGW-Probing-5xx"
+
+  log_group_names = [aws_cloudwatch_log_group.apigw_access_logs.name]
+
+  query_string = <<-EOT
+    fields @timestamp, @message
+    | filter apigwId = "${module.probing_apigw.apigw_id}"
+    | filter status like /5./
+    | sort @timestamp desc
+  EOT
+}
+
+resource "aws_cloudwatch_query_definition" "apigw_waf_block" {
+  name = "APIGW-Probing-WAF-Block"
+
+  log_group_names = [aws_cloudwatch_log_group.apigw_access_logs.name]
+
+  query_string = <<-EOT
+    fields @timestamp, @message
+    | filter apigwId = "${module.probing_apigw.apigw_id}"
+    | filter wafStatus != "200"
+    | sort @timestamp desc
+  EOT
+}
+
+resource "aws_cloudwatch_query_definition" "apigw_extended_request_id" {
+  name = "APIGW-Probing-ExtendedRequestId"
+
+  log_group_names = [aws_cloudwatch_log_group.apigw_access_logs.name]
+
+  query_string = <<-EOT
+    SELECT APIGW.`@timestamp` as apigw_timestamp, APIGW.`@message` as apigw_message, WAF.`@message` as waf_message, WAF.`@timestamp` as waf_timestamp
+    FROM "${aws_cloudwatch_log_group.apigw_access_logs.name}" as APIGW
+    INNER JOIN "${aws_cloudwatch_log_group.probing_waf.name}" as WAF ON APIGW.`requestId` = WAF.`httpRequest.requestId`
+    WHERE APIGW.extendedRequestId = ''
+  EOT
+}
