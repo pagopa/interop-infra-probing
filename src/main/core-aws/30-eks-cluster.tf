33ef3ee7013486c49c1fa9d5201fab941124ac28
Manuel Morini <manuel.morini@digitouch.it>
2026-07-09 12:16:36 +0200

    upgrade Terraform required version and AWS provider version

--- src/main/core-aws/30-eks-cluster.tf
+++ src/main/core-aws/30-eks-cluster.tf
@@ -33,20 +33,20 @@ module "eks" {
   depends_on = [aws_iam_role.deployment_github_repo]
 
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.11.0"
+  version = "~> 21.0"
 
-  cluster_name    = format("%s-eks-cluster-%s", local.project, var.env)
-  cluster_version = var.eks_k8s_version
+  name               = format("%s-eks-cluster-%s", local.project, var.env)
+  kubernetes_version = var.eks_k8s_version
 
   vpc_id                   = data.aws_vpc.probing.id
   control_plane_subnet_ids = data.aws_subnets.eks_control_plane.ids
-  cluster_ip_family        = "ipv4"
+  ip_family                = "ipv4"
   # CIDR range for K8s services IPs. Just to avoid potential overlap with other networks
-  cluster_service_ipv4_cidr = "10.1.0.0/21"
+  service_ipv4_cidr = "10.1.0.0/21"
 
   node_security_group_enable_recommended_rules = false
 
-  cluster_security_group_additional_rules = {
+  security_group_additional_rules = {
     from_github_runners = {
       type                     = "ingress"
       from_port                = 0
@@ -65,9 +65,9 @@ module "eks" {
     }
   }
 
-  cluster_endpoint_public_access  = false
-  cluster_endpoint_private_access = true
-  authentication_mode             = "API_AND_CONFIG_MAP"
+  endpoint_public_access  = false
+  endpoint_private_access = true
+  authentication_mode     = "API_AND_CONFIG_MAP"
 
   access_entries = merge(
     {
@@ -104,13 +104,13 @@ module "eks" {
     }
   )
 
-  cluster_security_group_use_name_prefix = false
-  cluster_security_group_name            = format("eks/cp-eni/%s-eks-cluster-%s", local.project, var.env)
+  security_group_use_name_prefix = false
+  security_group_name            = format("eks/cp-eni/%s-eks-cluster-%s", local.project, var.env)
 
   iam_role_use_name_prefix = false
   iam_role_name            = format("%s-eks-cluster-%s", local.project, var.env)
 
-  cluster_enabled_log_types              = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+  enabled_log_types                      = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
   cloudwatch_log_group_retention_in_days = var.env == "prod" ? 365 : 90
 
   kms_key_enable_default_policy = true
@@ -118,37 +118,48 @@ module "eks" {
     data.aws_iam_role.sso_admin.arn
   ]
 
-  fargate_profile_defaults = {
-    subnet_ids               = data.aws_subnets.eks_workload.ids
-    iam_role_use_name_prefix = false
-
-    iam_role_additional_policies = {
-      fargate_logging = aws_iam_policy.fargate_profile_logging.arn
-    }
-  }
-
   fargate_profiles = merge({
     system = {
+      subnet_ids = data.aws_subnets.eks_workload.ids
       name      = format("%s-fargate-system-profile-%s", local.project, var.env)
       selectors = [for ns in local.system_namespaces : { namespace = ns }]
+      iam_role_use_name_prefix = false
+      iam_role_additional_policies = {
+        fargate_logging = aws_iam_policy.fargate_profile_logging.arn
+      }
     }
     application = {
+      subnet_ids = data.aws_subnets.eks_workload.ids
       name      = format("%s-fargate-app-profile-%s", local.project, var.env) # e.g. the "probing-fargate-app-profile-dev" schedules on fargate all the pods in the application namespaces (dev, qa, vapt) in the dev cluster.
       selectors = [for ns in local.application_namespaces : { namespace = ns }]
+      iam_role_use_name_prefix = false
+      iam_role_additional_policies = {
+        fargate_logging = aws_iam_policy.fargate_profile_logging.arn
+      }
     }
     observability = {
+      subnet_ids = data.aws_subnets.eks_workload.ids
       name      = format("%s-fargate-obs-profile-%s", local.project, var.env)
       selectors = [for ns in local.observability_namespaces : { namespace = ns }]
+      iam_role_use_name_prefix = false
+      iam_role_additional_policies = {
+        fargate_logging = aws_iam_policy.fargate_profile_logging.arn
+      }
     } },
     length(local.tools_namespaces) > 0 ? ({
       tools = {
+        subnet_ids = data.aws_subnets.eks_workload.ids
         name      = format("%s-fargate-tools-profile-%s", local.project, var.env)
         selectors = [for ns in local.tools_namespaces : { namespace = ns }]
+        iam_role_use_name_prefix = false
+        iam_role_additional_policies = {
+          fargate_logging = aws_iam_policy.fargate_profile_logging.arn
+        }
       }
     }) : {}
   )
 
-  cluster_addons = {
+  addons = {
     vpc-cni = {
       addon_version               = var.eks_vpc_cni_version
       most_recent                 = false # use 'default' version for the K8s version
