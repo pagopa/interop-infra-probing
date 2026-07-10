0a1ac3f65bf42335acefc7e4ebb9674c963d4c65
Manuel Morini <manuel.morini@digitouch.it>
2026-07-10 11:32:28 +0200

    update version constraints

--- src/init/main.tf
+++ src/init/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.15.7"
+  required_version = "~> 1.15"
 
   # TODO Uncomment once the backend S3 bucket is created and upload the state file.
   backend "s3" {}
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.53.0"
+      version = "~> 6.53"
     }
   }
 }
--- src/main/core-k8s/00-main.tf
+++ src/main/core-k8s/00-main.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = "~> 1.15.7"
+  required_version = "~> 1.15"
 
   backend "s3" {}
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.53.0"
+      version = "~> 6.53"
     }
 
     kubernetes = {
