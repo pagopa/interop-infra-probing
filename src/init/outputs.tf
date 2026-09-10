05f7d95fb0fd51e8e43dbf6db0c4d3207c7f4c53
Manuel Morini <manuel.morini@digitouch.it>
2026-09-10 10:55:22 +0200

    remove dynamodb-terraform-state-lock resource

--- src/init/outputs.tf
+++ src/init/outputs.tf
@@ -3,11 +3,6 @@ output "backend_bucket_name" {
   description = "Name of the S3 bucket used for storing Terraform state files."
 }
 
-output "dynamodb_lock_table" {
-  value       = aws_dynamodb_table.dynamodb-terraform-state-lock.name
-  description = "Name of the DynamoDB table used for Terraform state locking."
-}
-
 output "iac_role_arn" {
   value       = aws_iam_role.githubiac.arn
   description = "Role to use in github actions to build the infrastructure."
